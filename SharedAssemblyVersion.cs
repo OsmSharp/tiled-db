12554eae19d4fed5d21051d809e54af2cd5fe048
xivk <ben@itinero.tech>
2020-05-05 15:42:51 +0100

    Bumped version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.29")]
-[assembly: AssemblyInformationalVersion("0.0.29-alpha")]
+[assembly: AssemblyVersion("0.0.30")]
+[assembly: AssemblyInformationalVersion("0.0.30-alpha")]
