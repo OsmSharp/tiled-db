8c73557fc9e338657513c82fef97fcdf1f9f29e3
xivk <ben@itinero.tech>
2020-05-19 20:20:12 +0100

    Updated OsmSharp.Binary.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.34")]
-[assembly: AssemblyInformationalVersion("0.0.34-alpha")]
+[assembly: AssemblyVersion("0.0.35")]
+[assembly: AssemblyInformationalVersion("0.0.35-alpha")]
